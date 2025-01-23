47738b2a431f9f3df260396a84a0063a0077c3a7
Sviatoslav <downadow@ro.ru>
2025-01-23 21:04:59 +0300

    root commit

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# TicTacToe
+
+Примитивная игра для Android и ПК. Автор — downadow.
+
+## Информация
+
+Данный проект сгенерирован с помощью [gdx-liftoff](https://github.com/libgdx/gdx-liftoff) версии `1.13.1.0`.
+Лицензия TicTacToe (включая часть данных шаблона и значки) и gdx-liftoff — Apache License 2.0.
+
+### gdx-liftoff: Credits
+
+The project was forked from the [`czyzby/gdx-setup`](https://github.com/czyzby/gdx-setup) repository.
+[@czyzby](https://github.com/czyzby) and [@kotcrab](https://github.com/kotcrab) created the original application,
+as well as a set of libraries that it depends on (`gdx-lml` and `VisUI` respectively). Since then, the project is
+maintained by [@tommyettinger](https://github.com/tommyettinger). Graciously, czyzby came back and made a wide variety of improvements, so big
+thanks there! Thanks also to [@metaphore](https://github.com/metaphore), who now maintains gdx-lml (which this used and may still use).
+
+[@raeleus](https://github.com/raeleus) created the
+[Particle Park skin for scene2d.ui](https://ray3k.wordpress.com/particle-park-ui-skin-for-scene2d-ui/),
+which was adapted to be the default skin added to new projects (if the _"Generate UI Assets"_ option is selected).
+"Accademia di Belle Arti di Urbino and students of MA course of Visual design" has created the _Titillium Web_
+font that the skin uses (under SIL OFL license). Oh yeah, and he did some other stuff starting in version 1.12.1.10,
+such as *almost the entire release*. Huge thanks to raeleus for the complete overhaul of the user experience!
+
+Other project contributors include [@Mr00Anderson](https://github.com/Mr00Anderson), [@lyze237](https://github.com/lyze237),
+[@metaphore](https://github.com/metaphore) (again!), and [@payne911](https://github.com/payne911).
+People who haven't directly contributed code have still helped a lot by spending their time to test on platforms
+like macOS and iOS; [@JojoIce](https://github.com/JojoIce) is one of several people who made a difference regarding iOS. And of course,
+many thanks go to all the early adopters for putting up with any partially-working releases early on!
+
+The randomized icons chopped up and used for Android projects come from the [OpenMoji](https://openmoji.org) project.
+If you want to use these icons in a less-mangled format, there's
+[openmoji-atlas](https://github.com/tommyettinger/openmoji-atlas) to access these emoji from libGDX conveniently.
+
+Thanks also to everyone who has made the various libraries and tools Liftoff depends upon. From the huge team
+responsible for Graal Native Image, to [Construo](https://github.com/fourlastor-alexandria/construo) by pretty much a
+[team of one](https://github.com/fourlastor), some of the best features of Liftoff aren't in Liftoff code at all.
---
+++ core/src/main/java/downadow/tictactoe/Main.java
@@ -0,0 +1,319 @@
+/*
+   Copyright 2025 downadow
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       https://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package downadow.tictactoe;
+
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.net.*;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.*;
+import com.badlogic.gdx.graphics.glutils.*;
+import com.badlogic.gdx.utils.*;
+import com.badlogic.gdx.utils.viewport.*;
+import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.Input.*;
+import java.util.Random;
+
+public class Main implements ApplicationListener {
+    SpriteBatch batch;
+    ShapeRenderer shape;
+    FitViewport viewport;
+    Vector2 touch;
+    final int X = 1, O = 2;
+    int next = X;
+    int[][] map = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+    Texture xTexture, oTexture;
+    
+    public void create() {
+        touch = new Vector2();
+        batch = new SpriteBatch();
+        shape = new ShapeRenderer();
+        viewport = new FitViewport(5, 4);
+        xTexture = new Texture("x.png");
+        oTexture = new Texture("o.png");
+        
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            public boolean touchDown(int x, int y, int ptr, int btn) {
+                touch.set(x, y);
+                viewport.unproject(touch);
+                
+                if(next == 0) {
+                    for(int i = 0; i < 3; i++) {
+                        for(int j = 0; j < 3; j++) {
+                            map[i][j] = 0;
+                        }
+                    }
+                    next = X;
+                    return true;
+                }
+                
+                /* ход бота */
+                else if(touch.x > 0f && touch.x < 0.6f && touch.y > 0f && touch.y < 0.6f && next == X) {
+                    generateNext();
+                    next = O;
+                    check();
+                    return true;
+                }
+                /* ввод */
+                else if(touch.x > 1f && touch.x < 2f && touch.y > 2.5f && touch.y < 3.5f && map[0][0] == 0) {
+                    map[0][0] = next;
+                    next = next == X ? O : X;
+                    check();
+                    return true;
+                } else if(touch.x > 2f && touch.x < 3f && touch.y > 2.5f && touch.y < 3.5f && map[0][1] == 0) {
+                    map[0][1] = next;
+                    next = next == X ? O : X;
+                    check();
+                    return true;
+                } else if(touch.x > 3f && touch.x < 4f && touch.y > 2.5f && touch.y < 3.5f && map[0][2] == 0) {
+                    map[0][2] = next;
+                    next = next == X ? O : X;
+                    check();
+                    return true;
+                } else if(touch.x > 1f && touch.x < 2f && touch.y > 1.5f && touch.y < 2.5f && map[1][0] == 0) {
+                    map[1][0] = next;
+                    next = next == X ? O : X;
+                    check();
+                    return true;
+                } else if(touch.x > 2f && touch.x < 3f && touch.y > 1.5f && touch.y < 2.5f && map[1][1] == 0) {
+                    map[1][1] = next;
+                    next = next == X ? O : X;
+                    check();
+                    return true;
+                } else if(touch.x > 3f && touch.x < 4f && touch.y > 1.5f && touch.y < 2.5f && map[1][2] == 0) {
+                    map[1][2] = next;
+                    next = next == X ? O : X;
+                    check();
+                    return true;
+                } else if(touch.x > 1f && touch.x < 2f && touch.y > 0.5f && touch.y < 1.5f && map[2][0] == 0) {
+                    map[2][0] = next;
+                    next = next == X ? O : X;
+                    check();
+                    return true;
+                } else if(touch.x > 2f && touch.x < 3f && touch.y > 0.5f && touch.y < 1.5f && map[2][1] == 0) {
+                    map[2][1] = next;
+                    next = next == X ? O : X;
+                    check();
+                    return true;
+                } else if(touch.x > 3f && touch.x < 4f && touch.y > 0.5f && touch.y < 1.5f && map[2][2] == 0) {
+                    map[2][2] = next;
+                    next = next == X ? O : X;
+                    check();
+                    return true;
+                }
+                
+                return false;
+            }
+        });
+        
+    }
+    
+    public void render() {
+        ScreenUtils.clear(Color.WHITE);
+        viewport.apply();
+        viewport.getCamera().update();
+        
+        shape.setProjectionMatrix(viewport.getCamera().combined);
+        shape.begin(ShapeRenderer.ShapeType.Line);
+        shape.setColor(0, 0, 0, 1);
+        shape.line(1f, 1.5f, 4f, 1.5f);
+        shape.line(1f, 2.5f, 4f, 2.5f);
+        shape.line(2f, 0.5f, 2f, 3.5f);
+        shape.line(3f, 0.5f, 3f, 3.5f);
+        shape.end();
+        
+        batch.setProjectionMatrix(viewport.getCamera().combined);
+        batch.begin();
+        
+        if(next == X)
+            batch.draw(xTexture, 0, 0, 0.6f, 0.6f);
+        else if(next == O)
+            batch.draw(oTexture, 0, 0, 0.6f, 0.6f);
+        
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(map[i][j] == X)
+                    batch.draw(xTexture, 1 + j, 4 - (0.5f + i) - 1, 1, 1);
+                else if(map[i][j] == O)
+                    batch.draw(oTexture, 1 + j, 4 - (0.5f + i) - 1, 1, 1);
+            }
+        }
+        batch.end();
+    }
+    
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
+    }
+    
+    public void pause()   {}
+    public void resume()  {}
+    public void dispose() {}
+    
+    
+    /* проверка */
+    private void check() {
+        boolean ok = false;
+        loop:
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(map[i][j] == 0) {
+                    ok = true;
+                    break loop;
+                }
+            }
+        }
+        if(!ok) next = 0;
+        
+        if(map[0][0] == X && map[0][1] == X && map[0][2] == X)
+            next = 0;
+        else if(map[1][0] == X && map[1][1] == X && map[1][2] == X)
+            next = 0;
+        else if(map[2][0] == X && map[2][1] == X && map[2][2] == X)
+            next = 0;
+        else if(map[0][0] == X && map[1][0] == X && map[2][0] == X)
+            next = 0;
+        else if(map[0][1] == X && map[1][1] == X && map[2][1] == X)
+            next = 0;
+        else if(map[0][2] == X && map[1][2] == X && map[2][2] == X)
+            next = 0;
+        else if(map[0][0] == X && map[1][1] == X && map[2][2] == X)
+            next = 0;
+        else if(map[0][2] == X && map[1][1] == X && map[2][0] == X)
+            next = 0;
+        
+        else if(map[0][0] == O && map[0][1] == O && map[0][2] == O)
+            next = 0;
+        else if(map[1][0] == O && map[1][1] == O && map[1][2] == O)
+            next = 0;
+        else if(map[2][0] == O && map[2][1] == O && map[2][2] == O)
+            next = 0;
+        else if(map[0][0] == O && map[1][0] == O && map[2][0] == O)
+            next = 0;
+        else if(map[0][1] == O && map[1][1] == O && map[2][1] == O)
+            next = 0;
+        else if(map[0][2] == O && map[1][2] == O && map[2][2] == O)
+            next = 0;
+        else if(map[0][0] == O && map[1][1] == O && map[2][2] == O)
+            next = 0;
+        else if(map[0][2] == O && map[1][1] == O && map[2][0] == O)
+            next = 0;
+    }
+    
+    /* ход бота */
+    private void generateNext() {
+        /* побеждаем */
+        
+        if(map[0][0] == X && map[1][1] == X && map[2][2] == 0) {
+            map[2][2] = X;
+            return;
+        } else if(map[0][0] == 0 && map[1][1] == X && map[2][2] == X) {
+            map[0][0] = X;
+            return;
+        } else if(map[0][0] == X && map[1][1] == 0 && map[2][2] == X) {
+            map[1][1] = X;
+            return;
+        } else if(map[0][2] == X && map[1][1] == X && map[2][0] == 0) {
+            map[2][0] = X;
+            return;
+        } else if(map[0][2] == 0 && map[1][1] == X && map[2][0] == X) {
+            map[0][2] = X;
+            return;
+        } else if(map[0][2] == X && map[1][1] == 0 && map[2][0] == X) {
+            map[1][1] = X;
+            return;
+        }
+        
+        for(int i = 0; i < 3; i++) {
+            if(map[i][0] == X && map[i][1] == X && map[i][2] == 0) {
+                map[i][2] = X;
+                return;
+            } else if(map[i][0] == 0 && map[i][1] == X && map[i][2] == X) {
+                map[i][0] = X;
+                return;
+            } else if(map[i][0] == X && map[i][1] == 0 && map[i][2] == X) {
+                map[i][1] = X;
+                return;
+            } else if(map[0][i] == X && map[1][i] == X && map[2][i] == 0) {
+                map[2][i] = X;
+                return;
+            } else if(map[0][i] == 0 && map[1][i] == X && map[2][i] == X) {
+                map[0][i] = X;
+                return;
+            } else if(map[0][i] == X && map[1][i] == 0 && map[2][i] == X) {
+                map[1][i] = X;
+                return;
+            }
+        }
+        
+        /* защита */
+        if(map[0][0] == O && map[1][1] == O && map[2][2] == 0) {
+            map[2][2] = X;
+            return;
+        } else if(map[0][0] == 0 && map[1][1] == O && map[2][2] == O) {
+            map[0][0] = X;
+            return;
+        } else if(map[0][0] == O && map[1][1] == 0 && map[2][2] == O) {
+            map[1][1] = X;
+            return;
+        } else if(map[0][2] == O && map[1][1] == O && map[2][0] == 0) {
+            map[2][0] = X;
+            return;
+        } else if(map[0][2] == 0 && map[1][1] == O && map[2][0] == O) {
+            map[0][2] = X;
+            return;
+        } else if(map[0][2] == O && map[1][1] == 0 && map[2][0] == O) {
+            map[1][1] = X;
+            return;
+        }
+        
+        for(int i = 0; i < 3; i++) {
+            if(map[i][0] == O && map[i][1] == O && map[i][2] == 0) {
+                map[i][2] = X;
+                return;
+            } else if(map[i][0] == 0 && map[i][1] == O && map[i][2] == O) {
+                map[i][0] = X;
+                return;
+            } else if(map[i][0] == O && map[i][1] == 0 && map[i][2] == O) {
+                map[i][1] = X;
+                return;
+            } else if(map[0][i] == O && map[1][i] == O && map[2][i] == 0) {
+                map[2][i] = X;
+                return;
+            } else if(map[0][i] == 0 && map[1][i] == O && map[2][i] == O) {
+                map[0][i] = X;
+                return;
+            } else if(map[0][i] == O && map[1][i] == 0 && map[2][i] == O) {
+                map[1][i] = X;
+                return;
+            }
+        }
+        
+        /* новый ход */
+        
+        int x = new Random().nextInt(3);
+        int y = new Random().nextInt(3);
+        if(map[y][x] != 0) {
+            generateNext();
+            return;
+        }
+        
+        map[y][x] = X;
+    }
+}
